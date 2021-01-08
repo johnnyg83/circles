a5c75c56d363e246e54eecba2ef54dab7b3149ee
Chris-Yao <yaochris01@gmail.com>
2021-01-08 01:16:33 -0500

    methods (told you so)

--- api/app/models.py
+++ api/app/models.py
@@ -8,6 +8,34 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+    def addInterest(self, interest):
+        if len(InterestsTable.query.filter_by(id=self.id, interest=interest).all()) == 0:
+            db.session.add(InterestsTable(id=self.id, interest=interest))
+            db.session.commit()
+
+    def getInterests(self):
+        return InterestsTable.query.filter_by(id=self.id).all()
+
+    def deleteInterest(self, interest):
+        row = InterestsTable.query.filter_by(id=self.id, interest=interest).first()
+        if row is not None:
+            db.session.delete(row)
+            db.session.commit()
+    
+    def addFriend(self, friend):
+        if len(FriendsTable.query.filter_by(id=self.id, friend_id=friend.id).all()) == 0:
+            db.session.add(FriendsTable(id=self.id, friend_id=friend.id))
+            db.session.commit()
+
+    def getFriends(self):
+        return FriendsTable.query.filter_by(id=self.id).all()
+
+    def deleteFriend(self, friend):
+        row = FriendsTable.query.filter_by(id=self.id, friend_id=friend.id).first()
+        if row is not None:
+            db.session.delete(row)
+            db.session.commit()
+
 
 class InterestsTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,3 +43,14 @@ class InterestsTable(db.Model):
 
     def __repr__(self):
         return '<InterestsTable %r %r>' % (self.id, self.interest)
+
+class FriendsTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    friend_id = db.Column(db.Integer, primary_key=True)
+
+    # if username=='johnny_g':
+    #     friends = 0
+
+    def __repr__(self):
+        return '<FriendsTable %r %r>' % (self.id, self.friend_id)
+
--- api/app/routes.py
+++ api/app/routes.py
@@ -7,41 +7,35 @@ from .models import InterestsTable
 def home():
     johnny_gundo = User(username='johnny_g', email='john.gunderson@yale.edu')
     chrissy_yaodo = User(username='chrissy_y', email='chris.yao@yale.edu')
+    db.drop_all()
     db.create_all()
+    db.session.commit()
     db.session.add(johnny_gundo)
     db.session.add(chrissy_yaodo)
     db.session.commit()
-    addUserInterest(johnny_gundo, "tennis")
-    addUserInterest(chrissy_yaodo, "being epic")
-    deleteUserInterest(chrissy_yaodo, "sucking")
-    print(getUserInterests(chrissy_yaodo))
 
-    addUserInterest(johnny_gundo, "tennis")
-    addUserInterest(johnny_gundo, "reading")
-    print(getUserInterests(johnny_gundo))
-    addUserInterest(johnny_gundo, "meat tenderizing")
-    print(getUserInterests(johnny_gundo))
-    deleteUserInterest(johnny_gundo, "meat tenderizing")
-    print(getUserInterests(johnny_gundo))
+    johnny_gundo.addFriend(chrissy_yaodo)
+    print(johnny_gundo.getFriends())
+    chrissy_yaodo.deleteFriend(johnny_gundo)
+    print(johnny_gundo.getFriends())
+
+    johnny_gundo.addInterest("tennis")
+    chrissy_yaodo.addInterest("being epic")
+    chrissy_yaodo.deleteInterest("sucking")
+    print(chrissy_yaodo.getInterests())
+
+    johnny_gundo.addInterest("tennis")
+    johnny_gundo.addInterest("reading")
+    print(johnny_gundo.getInterests())
+
+    johnny_gundo.addInterest("meat tenderizing")
+    print(johnny_gundo.getInterests())
+
+    johnny_gundo.deleteInterest("meat tenderizing")
+    print(johnny_gundo.getInterests())
 
     return render_template('home.html')
 
-def addUserInterest(user, interest):
-    if len(InterestsTable.query.filter_by(id=user.id, interest=interest).all()) == 0:
-        db.session.add(InterestsTable(id=user.id, interest=interest))
-        db.session.commit()
-
-def addItemToTable():
-    pass
-
-def getUserInterests(user):
-    return InterestsTable.query.filter_by(id=user.id).all()
-
-def deleteUserInterest(user, interest):
-    row = InterestsTable.query.filter_by(id=user.id, interest=interest).first()
-    if row is not None:
-        db.session.delete(row)
-        db.session.commit()
 
 @app.route('/about')
 def about():
