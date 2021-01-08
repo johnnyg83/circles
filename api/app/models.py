6adf8b64009501da30b556dfd4746ec143a7d773
Chris-Yao <yaochris01@gmail.com>
2021-01-08 00:33:27 -0500

    interest table

--- api/app/models.py
+++ api/app/models.py
@@ -4,6 +4,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    interests = db.Column(db.String(1000))
+
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class InterestsTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    interest = db.Column(db.String(1000), primary_key=True)
+
+    def __repr__(self):
+        return '<InterestsTable %r %r>' % (self.id, self.interest)
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,12 +1,43 @@
 from . import app, db
-from.models import User
+from .models import User
+from .models import InterestsTable
 
 @app.route('/')
 def home():
     johnny_gundo = User(username='johnny_g', email='john.gunderson@yale.edu')
+    chrissy_yaodo = User(username='chrissy_y', email='chris.yao@yale.edu')
     db.create_all()
     db.session.add(johnny_gundo)
+    db.session.add(chrissy_yaodo)
     db.session.commit()
-    print(User.query.filter_by(username='johnny_g').first())
+    addUserInterest(johnny_gundo, "tennis")
+    addUserInterest(chrissy_yaodo, "being epic")
+    deleteUserInterest(chrissy_yaodo, "sucking")
+    print(getUserInterests(chrissy_yaodo))
+
+    addUserInterest(johnny_gundo, "tennis")
+    addUserInterest(johnny_gundo, "reading")
+    print(getUserInterests(johnny_gundo))
+    addUserInterest(johnny_gundo, "meat tenderizing")
+    print(getUserInterests(johnny_gundo))
+    deleteUserInterest(johnny_gundo, "meat tenderizing")
+    print(getUserInterests(johnny_gundo))
 
     return 'Hello World!'
+
+def addUserInterest(user, interest):
+    if len(InterestsTable.query.filter_by(id=user.id, interest=interest).all()) == 0:
+        db.session.add(InterestsTable(id=user.id, interest=interest))
+        db.session.commit()
+
+def getUserInterests(user):
+    return InterestsTable.query.filter_by(id=user.id).all()
+
+def deleteUserInterest(user, interest):
+    row = InterestsTable.query.filter_by(id=user.id, interest=interest).first()
+    if row is not None:
+        db.session.delete(row)
+        db.session.commit()
+
+
+
