2c0dce4c50bdc66b913c4006f914304dbdd3deec
Chris-Yao <yaochris01@gmail.com>
2021-01-08 00:42:14 -0500

    Merge branch 'master' of https://github.com/johnnyg83/circles into master

--- api/app/api.py
+++ api/app/api.py
@@ -1,4 +1,27 @@
 from flask import Blueprint
-
+from util import fail, succ
 api_bp = Blueprint('api', __name__)
+
+@api_bp.errorhandler(404)
+def not_found(error):
+    return fail('Not found.', 404)
+
+
+@api_bp.errorhandler(401)
+def unauthorized(error):
+    return fail('You\'re not authorized to perform this action.', 401)
+
+
+@api_bp.errorhandler(403)
+def forbidden(error):
+    return fail('You don\'t have permission to do this.', 403)
+
+
+@api_bp.errorhandler(500)
+def internal(error):
+    return fail('Internal server error.', 500)
+
+@api_bp.route('/')
+def home():
+    return "Hello API!"
 
--- api/app/config.py
+++ api/app/config.py
@@ -1,4 +1,4 @@
 import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
